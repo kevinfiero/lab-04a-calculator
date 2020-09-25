7ad918fec855375cba4a54ccae673661d1166d71
kevinfiero <kjf46@cornell.edu>
2020-09-24 17:50:57 -0700

    fixed errors from unit test

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 // import functions and grab DOM elements
 
-import { calculate } from "./clickHandlers.js";
+import { calculate } from './clickHandlers.js';
 
 const calcVal = document.getElementById('calculate');
 
-console.log(calcVal);
 // set event listeners to update state and DOM
 
 calcVal.addEventListener('click', calculate);
--- clickHandlers.js
+++ clickHandlers.js
@@ -1,8 +1,5 @@
 import { logic, resetValues } from "./mathUtils.js";
 
-const firstVal = document.getElementById('first');
-const secondVal = document.getElementById('second');
-const operatorVal = document.getElementById('operator');
 const outputVal = document.getElementById('output');
 
 
