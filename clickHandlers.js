d9464692e53a0d638c5d7e25700568efc0c14fd5
kevinfiero <kjf46@cornell.edu>
2020-09-25 11:27:21 -0700

    fixed comment

--- clickHandlers.js
+++ clickHandlers.js
@@ -6,7 +6,7 @@ const secondVal = document.getElementById('second');
 const operatorVal = document.getElementById('operator');
 
 export function calculate() { 
-    const indexVal = operatorVal.options[operatorVal.selectedIndex].value; //returns index value of the options box + = 0, - = 1, x =3, / = 4
+    const indexVal = operatorVal.options[operatorVal.selectedIndex].value; //returns index value of the options box + = 0, - = 1, x = 2, / = 3
     outputVal.textContent = logic(firstVal.value, secondVal.value, indexVal); // passes two values and the type of operation, returns result and logs to HTML element
     firstVal.value = ''; //clears out first text box value
     secondVal.value = ''; //clears out second text box value
