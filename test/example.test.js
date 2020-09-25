e7b1ed587cb5ef210998379ff7b1c49ef3cece3a
kevinfiero <kjf46@cornell.edu>
2020-09-24 21:28:48 -0700

    added unit tests

--- test/example.test.js
+++ test/example.test.js
@@ -2,21 +2,85 @@
 
 import { logic } from '../mathUtils.js';
 
-// import { example } from '../example.js';
-
 const test = QUnit.test;
 
 test('add 3 + 10 = 13', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     
-    
-    const firstVal = Number(3);
-    const secondVal = Number(10);
-    const indexVal = Number(0); //signifies addition operation index
-    const expectedVal = Number(13);
+    const firstVal = 3;
+    const secondVal = 10;
+    const indexVal = 0; //signifies addition operation index
+    const expectedVal = 13;
 
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = logic(firstVal, secondVal, indexVal);
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(result, expectedVal);
+});
+
+test('subtract 10 - 3 = 7', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
     
+    const firstVal = 10;
+    const secondVal = 3;
+    const indexVal = 1; //signifies subtraction operation index
+    const expectedVal = 7;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = logic(firstVal, secondVal, indexVal);
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(result, expectedVal);
+});
+
+test('multiply 10 * 3 = 30', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    
+    const firstVal = 10;
+    const secondVal = 3;
+    const indexVal = 2; //signifies multiply operation index
+    const expectedVal = 30;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = logic(firstVal, secondVal, indexVal);
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(result, expectedVal);
+});
+
+test('divide 15 / 3 = 5', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    
+    const firstVal = 15;
+    const secondVal = 3;
+    const indexVal = 3; //signifies divide operation index
+    const expectedVal = 5;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = logic(firstVal, secondVal, indexVal);
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(result, expectedVal);
+});
+
+test('error handling strings', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    
+    const firstVal = 'p';
+    const secondVal = 'q';
+    const indexVal = 0; //signifies addition operation index
+    const expectedVal = "ERROR";
+
     //Act 
     // Call the function you're testing and set the result to a const
     const result = logic(firstVal, secondVal, indexVal);
